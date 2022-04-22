1477469a8bda3f4d84632961836e3a2e8cf6f368
Will Passidomo <willpassidomo@users.noreply.github.com>
2022-04-22 15:30:15 -0400

    ci: add semantic release (#14)

---
+++ release.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  branches: ["main"],
+  tagFormat: "v${version}",
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "angular",
+        releaseRules: [
+            {type: 'feat', release: 'minor'},
+            {type: 'ci', release: 'patch'},
+            {type: 'fix', release: 'patch'},
+            {type: 'docs', release: 'patch'},
+            {type: 'test', release: 'patch'},
+            {type: 'refactor', release: 'patch'},
+            {type: 'style', release: 'patch'},
+            {type: 'build', release: 'patch'},
+            {type: 'chore', release: 'patch'},
+            {type: 'revert', release: 'patch'}
+       ]
+      }
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "angular",
+      },
+    ],
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd: "sh ./.scripts/release.sh ${nextRelease.version}",
+      },
+    ],
+    ["@semantic-release/github"],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "gradle.properties", "README.md"],
+        message:
+          "chore(release): ${nextRelease.version} \n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
